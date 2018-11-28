21b0f41d9fa30640797cf290c3691c85f4e74b02
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-11-28 15:47:36 +0000

    adds api call for outdoor temperature

--- jasmine-standalone-3.3.0 (1)/src/Interface.js
+++ jasmine-standalone-3.3.0 (1)/src/Interface.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.current_temp + "\xB0C");
   showName();
+  updateOutdoorTemp();
 
   function showName() {
   	$('#powersaving').text(thermostat.power_saving_mode === true ? "on" : "off");
@@ -11,6 +12,12 @@ $(document).ready(function() {
   	$('#temperature').text(thermostat.current_temp + "\xB0C");
   	thermostat.set_energy_usage(thermostat.current_temp);
   	$('#temperature').attr('class', thermostat.energy_usage);
+  }
+
+  function updateOutdoorTemp() {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=8194a98a5ec3dd33445879c80f199595&units=metric', function(data) {
+      $("#outside-temp").text(data.main.temp + "\xB0C");
+    })
   }
 
   $('#temperature-up').click(function() {
